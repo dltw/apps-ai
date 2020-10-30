070f3286f523d227ac12b1df4f695a042301e0d2
David Leong <57188928+dltw@users.noreply.github.com>
2020-10-30 17:03:36 +0800

    Add lab3 code

---
+++ lab3_code/Inst3.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inst3 : MonoBehaviour
+{
+    public GameObject prefab;
+    public int height = 3;
+    public int width = 8;
+    public Vector3 location = Vector3.zero;
+
+    void Start()
+    {
+        location.z = 0.0f;
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                Instantiate(prefab, new Vector3(
+                    x + (int)location.x,
+                    y + (int)location.y,
+                    0.0f),
+                    Quaternion.identity);
+            }
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
