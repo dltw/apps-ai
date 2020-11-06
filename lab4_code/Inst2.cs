5dd5653dca611e76029b1ab5305a14e5be646b92
David Leong <57188928+dltw@users.noreply.github.com>
2020-11-06 17:09:41 +0800

    Add files for lab exercises

---
+++ lab4_code/Inst2.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inst2 : MonoBehaviour
+{
+    public Transform prefab;
+
+    void Start()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Vector3 position = new Vector3(
+                Random.Range(-8f, 8f),
+                0.0f,
+                Random.Range(-8f, 8f)
+            );
+
+            Instantiate(prefab, position, Quaternion.identity);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ lab4_code/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 8f;
+    private Vector3 target;
+
+    void Start()
+    {
+        target = transform.position;
+    }
+
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+        target = transform.position; // 8-ways direction
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            target += Vector3.left;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            target += Vector3.right;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            target += Vector3.forward;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            target += Vector3.back;
+        }
+
+        target.y = 0.0f;
+        transform.position = Vector3.MoveTowards(transform.position, target, step);
+    }
+}
