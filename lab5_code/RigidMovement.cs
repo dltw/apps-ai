bd3950d45de2200c7dd4a088f6538026d426f2d5
David Leong <57188928+dltw@users.noreply.github.com>
2020-11-11 17:07:16 +0800

    Add files for lab exercises

---
+++ lab5_code/RigidMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float applyForce = 1500f;
+    public float dampingForce = 0.15f;
+
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(-applyForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(applyForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(0, 0, applyForce * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(0, 0, -applyForce * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * dampingForce, ForceMode.Impulse);
+        }
+    }
+}
