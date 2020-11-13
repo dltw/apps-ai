360f6dcebd602f8cea0a88211834750e9386291f
David Leong <57188928+dltw@users.noreply.github.com>
2020-11-13 15:58:01 +0800

    Add files for lab exercises

---
+++ lab6_code/CharMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharMovement : MonoBehaviour
+{
+    CharacterController controller;
+    public float speed = 3.0f;
+    public float rotateSpeed = 3.0f;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        float axisH = Input.GetAxis("Horizontal"); // Keys A, D, left, right
+        float axisV = Input.GetAxis("Vertical"); // Keys W, S, up, down
+        float currentSpeed = axisV * speed;
+
+        Vector3 forwardDirection = transform.TransformDirection(Vector3.forward);
+        transform.Rotate(0, axisH * rotateSpeed, 0);
+
+        controller.SimpleMove(forwardDirection * currentSpeed);
+    }
+}
---
+++ lab6_code/CharMovement2.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharMovement2 : MonoBehaviour
+{
+    CharacterController controller;
+    Vector3 velocity;
+    bool playerGrounded;
+    public float speed = 3.0f;
+    public float rotateSpeed = 3.0f;
+    public float jumpHeight = 3.0f;
+    float gravity = -9.81f;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        float axisH = Input.GetAxis("Horizontal"); // Keys A, D, left, right
+        float axisV = Input.GetAxis("Vertical"); // Keys W, S, up, down
+        float currentSpeed = axisV * speed * Time.deltaTime;
+        Vector3 forwardDirection = transform.TransformDirection(Vector3.forward);
+        transform.Rotate(0, axisH * rotateSpeed, 0);
+
+        playerGrounded = controller.isGrounded;
+        if (playerGrounded && velocity.y < 0.0f)
+        {
+            velocity.y = 0.0f; // lock to y-axis origin
+        }
+
+        controller.Move(forwardDirection * currentSpeed);
+
+        if (Input.GetButtonDown("Jump")) // Keys spacebar
+        {
+            velocity.y += Mathf.Sqrt(Mathf.Abs(-jumpHeight * gravity)); // jump
+        }
+
+        velocity.y += gravity * Time.deltaTime; // fall
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
