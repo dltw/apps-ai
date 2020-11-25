996bfb8c14def19cb68ca46f3fbe7f836d4b3c6d
David Leong <57188928+dltw@users.noreply.github.com>
2020-11-25 17:20:17 +0800

    Add files for lab exercises

---
+++ lab8_code/Patrol.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Patrol : MonoBehaviour
+{
+    public Transform[] wayPointArray;
+    private NavMeshAgent agent;
+    int index = 0;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.autoBraking = false;
+        AgentPatrol();
+    }
+
+    void FixedUpdate()
+    {
+        if (!agent.pathPending && agent.remainingDistance < 1.5f)
+        {
+            AgentPatrol();
+        }
+    }
+
+    void AgentPatrol()
+    {
+        if (wayPointArray.Length == 0)
+        {
+            agent.isStopped = true;
+        }
+        else
+        {
+            agent.isStopped = false;
+        }
+
+        agent.destination = wayPointArray[index].position;
+        index++;
+        if (index >= wayPointArray.Length)
+        {
+            index = 0;
+        }
+    }
+}
